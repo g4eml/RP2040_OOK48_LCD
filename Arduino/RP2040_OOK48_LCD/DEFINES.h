8cb2d1cb2e4d87f255171cf99d21525a019128e2
g4eml <colin@durbridge.me.uk>
2026-08-22 21:53:00 +0100

    improve battery voltage setting

--- Arduino/RP2040_OOK48_LCD/DEFINES.h
+++ Arduino/RP2040_OOK48_LCD/DEFINES.h
@@ -1,4 +1,4 @@
-#define VERSION "Version 0.35"
+#define VERSION "Version 0.36"
 
 #define GPSTXPin 4                      //Serial data to GPS module 
 #define GPSRXPin 5                      //Serial data from GPS module
